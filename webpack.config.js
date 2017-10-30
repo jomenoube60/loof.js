16d0de4dff8f06b2867e9ce4ed4b9d899c561071
fab <fdev31@gmail.com>
2017-10-30 13:03:29 +0100

    update webpack config for dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 var path = require('path');
+//const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = {
     entry: './lib/main.js',
     output: {
         path: __dirname,
         filename: 'main.js'
     },
+    devtool: 'inline-source-map',
     module: {
         loaders: [
-            { loader: 'babel-loader', options: {          plugins: [require('babel-plugin-transform-object-rest-spread')]
-} }
+//            { loader: 'babel-loader', options: {          plugins: [require('babel-plugin-transform-object-rest-spread')] } }
         ]
-    }
+    },
+ plugins: [
+//   new UglifyJSPlugin()
+ ]
 };
